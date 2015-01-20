12e794e0788a4c882d1be24d5a631f097f1257f8
ChingHua <bbr5359@gmail.com>
2015-01-20 15:50:42 +0800

    LocalNotification

--- runningCounter/AppDelegate.h
+++ runningCounter/AppDelegate.h
@@ -12,6 +12,8 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (weak, nonatomic) UILocalNotification *notify;
+-(void)ReOderBadgeNumber;
 
 @end
 
--- runningCounter/AppDelegate.m
+++ runningCounter/AppDelegate.m
@@ -14,10 +14,36 @@
 
 @implementation AppDelegate
 
+-(void)ReOderBadgeNumber { //重新安排應用程式的按鈕的提示數字
+    //查看目前剩餘的通知數
+    NSArray *notifys = [[UIApplication sharedApplication] scheduledLocalNotifications];
+    int index = 1;
+    for (_notify in notifys) {
+        
+        _notify.applicationIconBadgeNumber = index; //安排新的badge number給每一筆資料
+        
+        [[UIApplication sharedApplication] scheduleLocalNotification:_notify]; //重新安排
+        index++;
+    }
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //裝置版本判斷，註冊LocalNotification
+    if ([[[UIDevice currentDevice] systemVersion] floatValue]>=8.0) {
+        if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]) {
+            [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound categories:nil ]];
+        }
+    }
+    
     return YES;
+}
+
+-(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+    //系統送過來的本地通知，ＡＰＰ執行時或使用中都會進來這邊
+    //或ＡＰＰ在背景，沒有使用時，點選橫幅．提醒都會進來
+    _notify.applicationIconBadgeNumber = 0;
+    [self ReOderBadgeNumber];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -28,18 +54,29 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    _notify.applicationIconBadgeNumber = 0;
+    [self ReOderBadgeNumber];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
+- (void)applicationWillEnterForeground:(UIApplication *)application {//程序到前景時
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    _notify.applicationIconBadgeNumber = 0;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    application.applicationIconBadgeNumber = 0;
+    [self ReOderBadgeNumber];
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void)applicationWillTerminate:(UIApplication *)application { //當程式被結束時
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+    localNotification.alertBody = @"You close App, can't step counter.";
+    localNotification.soundName = UILocalNotificationDefaultSoundName;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+
 }
 
 @end
