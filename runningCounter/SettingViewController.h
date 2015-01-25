ba038fba4528b1b9758e01fc65395b2a1fff400d
Ja7423 <snake9725074@gmail.com>
2015-01-25 21:20:10 +0800

    add
    1.add game 2,3
    2.change setting view
    3. add new map view controller

---
+++ runningCounter/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  runningCounter
+//
+//  Created by chiawei on 2015/1/25.
+//  Copyright (c) 2015年 Longfatown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
